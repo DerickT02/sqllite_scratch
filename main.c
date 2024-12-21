c82f48327e84d7cd653de639073733f2a5d6fcf0
DerickT02 <derickt18@outlook.com>
2024-12-20 16:21:21 -0800

    added minor fixes related to error handling

--- main.c
+++ main.c
@@ -12,8 +12,8 @@
 
 typedef struct {
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
@@ -59,8 +59,11 @@ typedef enum{
 
 typedef enum{
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
+
     PREPARE_UNRECOGNIZED_STATEMENT,
-    PREPARE_SYNTAX_ERROR
+    PREPARE_SYNTAX_ERROR,
+     PREPARE_STRING_TOO_LONG
 } PrepareResult;
 
 
@@ -69,6 +72,7 @@ typedef enum{
 InputBuffer * new_input_buffer();
 MetaCommandResult do_meta_command(InputBuffer * input_buffer, Table * table);
 PrepareResult prepare_statement(InputBuffer * input_buffer, Statement * statement);
+PrepareResult prepare_insert(InputBuffer * input_buffer, Statement * statement);
 
 //void functions
 void print_prompt(){printf("db > ");};
@@ -166,11 +170,17 @@ int main(int argc, char * argv[]){
 
                 printf("Unrecognized Keyword at start of '%s'. \n", input_buffer->buffer);
                 continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
+            case(PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
         }
         
         switch(execute_statement(&statement, table)){
             case(EXECUTE_SUCCESS):
-                printf("Executed\n");
+                printf("Executed.\n");
                 break;
             case(EXECUTE_TABLE_FULL):
                 printf("Table full\n");
@@ -202,17 +212,37 @@ MetaCommandResult do_meta_command(InputBuffer * input_buffer, Table * table){
     }
 };
 
+PrepareResult prepare_insert(InputBuffer * input_buffer, Statement * statement){
+    statement->type = STATEMENT_INSERT;
+    char * keyword = strtok(input_buffer->buffer, " ");
+    char * id_string = strtok(NULL, " ");
+    char * username = strtok(NULL, " ");
+    char * email = strtok(NULL, " ");
+    if(id_string == NULL || username == NULL || email == NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+    int id = atoi(id_string);
+    if(id < 0){
+        return PREPARE_NEGATIVE_ID;
+    }
+    if(strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if(strlen(email) > COLUMN_EMAIL_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 //Handles database commands and returns either a successful or unsuccessful execution
 PrepareResult prepare_statement(InputBuffer * input_buffer, Statement * statement){
     //checks if the first 6 characters spell insert, since there can be other characters following the insert command
     if(strncmp(input_buffer->buffer, "insert", 6) == 0){
-        statement->type = STATEMENT_INSERT;
-        //looking at the word insert first, and then it will look at the digit, and then the 2 strings
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-        if(args_assigned < 3){
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if(strcmp(input_buffer->buffer, "select") == 0){
         statement->type = STATEMENT_SELECT;
